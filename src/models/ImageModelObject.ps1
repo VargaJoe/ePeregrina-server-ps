27f1f9355620047c3ea23c536c8c727bb0df2dd2
vargajoe <varga.yosh@gmail.com>
2024-04-12 22:21:49 +0200

    image pager

--- src/models/ImageModelObject.ps1
+++ src/models/ImageModelObject.ps1
@@ -4,6 +4,7 @@ class ImageModelObject {
     ImageModelObject($requestObject) {
         $this.model = @{
             type = "image"
+            pager = $this.GetPager($requestObject)
             image = $this.GetImageData($requestObject.ContextPath)
         }        
     }
@@ -18,5 +19,38 @@ class ImageModelObject {
         }
     
         return $imageModel
+    }
+
+    [PSCustomObject]GetPager($requestObject){
+        $ImagePath = $requestObject.ContextPath 
+        $relativeContainerPath = (Split-Path -Path $requestObject.LocalPath -Parent) -replace "\\", "/"
+
+        $containerPath = [System.IO.Path]::GetDirectoryName($ImagePath)
+        $imageName = [System.IO.Path]::GetFileName($imagePath)
+        $pagerItems = Get-ChildItem -Path $containerPath | ForEach-Object {
+            $relUrlPath = $relativeContainerPath + "/" + "$($_.Name)"
+            
+            New-Object PSObject -Property @{
+                Name = $_.Name 
+                Url  = $relUrlPath
+            }
+        }
+
+
+        $currentIndex = -1
+        foreach ($item in $pagerItems) {
+            $currentIndex = $currentIndex + 1
+            if ($item.Name -eq $imageName) {
+                break
+            } 
+        }
+        $prevIndex = $currentIndex - 1
+        $nextIndex = $currentIndex + 1
+        $pager = @{
+            prev = $prevIndex -ge 0 ? $pagerItems[$prevIndex] : $null
+            next = $nextIndex -lt $pagerItems.Count ? $pagerItems[$nextIndex] : $null
+        }
+
+        return $pager
     }
 }
