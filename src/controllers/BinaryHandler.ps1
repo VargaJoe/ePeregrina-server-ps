51a3be442b2f488a6dac49caa2bb6a600198dc88
vargajoe <varga.yosh@gmail.com>
2024-04-14 12:45:13 +0200

    epub handlers (but resources still flawed

--- src/controllers/BinaryHandler.ps1
+++ src/controllers/BinaryHandler.ps1
@@ -1,13 +1,59 @@
 function BinaryHandler($requestObject) {
-    $fullPath = Get-FilePath $requestObject
+    Write-Host "BinaryHandler"
+    Show-Context
+    # $fullPath = $(Join-Path $Global:RootPath $requestObject.Settings.webFolder $requestObject.LocalPath)
+    $fullPath = $this.ContextPath
     Write-Output "fullpath: $($fullPath)"
 
-    "new response $fullpath" | Out-File -Append -FilePath "./log.txt"
+    # "new response $fullpath" | Out-File -Append -FilePath "./log.txt"
     $response = [ResponseObject]::new($requestObject.HttpContext.Response)
     $response.ResponseType = "binary"
-    if (Test-Path $fullPath) { 
+    # if (Test-Path $fullPath) { 
         $response.ResponseString = $Null
         $response.FilePath = Resolve-Path $fullPath
+    # } else { 
+    #     $response.ResponseString = $Null
+    #     $response.FilePath = $Null
+    #     $response.HttpResponse.StatusCode = 404
+    # }
+    
+    $response.Respond()
+    # "after response $fullpath" | Out-File -Append -FilePath "./log.txt"
+}
+
+function VirtualBinaryHandler($requestObject) {
+    Write-Host "VirtualBinaryHandler"
+    Show-Context
+    # "new response $fullpath" | Out-File -Append -FilePath "./log.txt"
+    $response = [ResponseObject]::new($requestObject.HttpContext.Response)
+    $response.ResponseType = "binary"
+    if ($requestObject.VirtualPath) { 
+        $selectedFile = $requestObject.VirtualPath.TrimStart('/')
+        $zipFile = $null
+        $stream = $null
+
+        try {
+            $zipFile = [System.IO.Compression.ZipFile]::OpenRead($requestObject.ContextPath)
+            $entry = $zipFile.GetEntry($selectedFile)
+        
+            if ($entry -eq $null) {
+                throw "File '$selectedFile' not found in zip file."
+            }
+        
+            $stream = $entry.Open()
+            $reader = New-Object System.IO.StreamReader($stream)
+            $response.ResponseString = $reader.ReadToEnd()
+        } finally {
+            $reader.Close()
+            
+            if ($null -ne $stream) {
+                $stream.Close()
+            }
+        
+            if ($null -ne $zipFile) {
+                $zipFile.Dispose()
+            }
+        }
     } else { 
         $response.ResponseString = $Null
         $response.FilePath = $Null
@@ -16,5 +62,5 @@ function BinaryHandler($requestObject) {
     }
     
     $response.Respond()
-    "after response $fullpath" | Out-File -Append -FilePath "./log.txt"
+    # "after response $fullpath" | Out-File -Append -FilePath "./log.txt"
 }
