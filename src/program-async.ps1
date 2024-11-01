d53bb12b7668b0c2039ec4c5882619f88d2b18ee
vargajoe <varga.yosh@gmail.com>
2024-11-01 16:02:45 +0100

    rework async as msxfaq webserverasync (still not statisfactory)

--- src/program-async.ps1
+++ src/program-async.ps1
@@ -2,6 +2,44 @@ param (
     [int]$ServicePort = 8898
 )
 
+function New-ScriptBlockCallback 
+    {
+        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
+        param(
+            [parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [scriptblock]$Callback
+        )
+
+        # Is this type already defined?
+        if (-not ( 'CallbackEventBridge' -as [type])) {
+            Add-Type @' 
+                using System; 
+ 
+                public sealed class CallbackEventBridge { 
+                    public event AsyncCallback CallbackComplete = delegate { }; 
+ 
+                    private CallbackEventBridge() {} 
+ 
+                    private void CallbackInternal(IAsyncResult result) { 
+                        CallbackComplete(result); 
+                    } 
+ 
+                    public AsyncCallback Callback { 
+                        get { return new AsyncCallback(CallbackInternal); } 
+                    } 
+ 
+                    public static CallbackEventBridge Create() { 
+                        return new CallbackEventBridge(); 
+                    } 
+                } 
+'@
+        }
+        $bridge = [callbackeventbridge]::create()
+        Register-ObjectEvent -InputObject $bridge -EventName callbackcomplete -Action $Callback -MessageData $args > $null
+        $bridge.Callback
+    }
+
 # Use the following commands to bind/unbind SSL cert
 # netsh http add sslcert ipport=0.0.0.0:443 certhash=3badca4f8d38a85269085aba598f0a8a51f057ae "appid={00112233-4455-6677-8899-AABBCCDDEEFF}"
 # netsh http delete sslcert ipport=0.0.0.0:443 
@@ -32,24 +70,20 @@ $Global:JsonResult = $null
 $Global:RootPath = $PSScriptRoot
 $Global:responseClosed = $true
 
+Write-host "Start Peregrina Webserver on Port $($ServicePort)"
 "new listener" | Out-File -Append -FilePath "./log.txt"
 $HttpListener = New-Object System.Net.HttpListener
-$HttpListener.Prefixes.Add("http://+:$($ServicePort)/")
-$HttpListener.Prefixes.Add("https://+:443/")
+$HttpListener.Prefixes.Add("http://+:"+$($ServicePort)+"/")
+# $HttpListener.Prefixes.Add("https://+:443/")
 $HttpListener.Start()
 
-$stopFile = "./appoffline.htm"
+Write-host "Start-WebserverAsync:Prepare RequestListener code"
+$requestListener = {
+            [cmdletbinding()]
+            param($result)
 
-# Function to handle incoming requests asynchronously
-function Handle-Request {
-    param (
-        [System.Net.HttpListenerContext]$context
-    )
-
-    $runspace = [powershell]::Create().AddScript({
-        param ($context)
-        try {
-            $Global:responseClosed = $false
+            [System.Net.HttpListener]$HttpListener = $result.AsyncState;
+            $context = $HttpListener.EndGetContext($result);    # waitfor request to complete
 
             # context variables
             if (-not $Global:responseClosed) {
@@ -75,40 +109,54 @@ function Handle-Request {
                 $requestObject = [ErrorRequestObject]::new($context)
                 $requestObject.RouteRequest()
             }
-        } catch {
-            Write-Error $_
-        } finally {
-            $context.Response.Close()
-        }
-    }).AddArgument($context).BeginInvoke()
-}
 
-# Function to begin listening for requests asynchronously
-function Begin-GetContext {
-    $HttpListener.BeginGetContext({
-        param ($result)
-        try {
-            $context = $HttpListener.EndGetContext($result)
-            Handle-Request -context $context
-        } catch {
-            Write-Error $_
-        } finally {
-            if ($HttpListener.IsListening -and !(Test-Path -Path $stopFile)) {
-                Begin-GetContext
+            # sample listener
+            $request = $context.Request                     # Collect Request
+            $response = $context.Response                   # get Response Object
+            write-host "got $($request.RawURL)"
+            switch ($request.RawURL) {                      # Handle Request based on the URL 
+                "/quit" {$HttpListener.Stop(); break}           # stop listener and exit handler
+                "/date" {$message = "WebServer Time $((get-date).DateTime)"}
+                Default {$message = "404 Page not found  use /quit to end webserver or /date to get system date";}
             }
-        }
-    }, $null)
-}
+            $response.ContentType = 'text/html';
+            [byte[]]$buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
+            $response.ContentLength64 = $buffer.length
+            $output = $response.OutputStream
+            $output.Write($buffer, 0, $buffer.length)
+            $output.Close()
+    } 
 
 try {
-    Begin-GetContext
+    $context = $HttpListener.BeginGetContext((New-ScriptBlockCallback -Callback $requestListener), $HttpListener)
+    Write-host "Start-WebserverAsync:Start Async listener Processing - Press any key to stop"
+    [datetime]$timestamp = Get-date
+    [long]$count = 60
 
-    # Keep the script running until the stop file is created
-    while ($HttpListener.IsListening -and !(Test-Path -Path $stopFile)) {
-        Start-Sleep -Seconds 1
-    }
+	$stopFile = "./appoffline.htm"
+
+	While ($HttpListener.IsListening -and !(Test-Path -Path $stopFile)) {
+		$Global:responseClosed = $false
+		Write-Host "`n`n"
+		If ($context.IsCompleted -and $HttpListener.IsListening) {
+            # start new Callback for next request. Do it at the start to handle quit events first     
+            $context = $HttpListener.BeginGetContext((New-ScriptBlockCallback -Callback $requestListener), $HttpListener)
+        }
+		
+        if ($count -ge 60) {
+            Write-host ; Write-Host "$(([System.DateTime]::UtcNow).tostring("u")) " -nonewline
+            $count=0
+        }
+        if ( ((get-date) - $timestamp).totalseconds -gt 1) {
+            write-host "." -NoNewline        
+            $count ++
+            $timestamp = Get-date
+        }
+	}
 }
 finally {
-    $HttpListener.Stop()
-    "stop listener" | Out-File -Append -FilePath "./log.txt"
+	$HttpListener.Stop()
+    $HttpListener.Close()
+    Write-host "Stop Pregerina Webserver"
+	"stop listener" | Out-File -Append -FilePath "./log.txt"
 }
